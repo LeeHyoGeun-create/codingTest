b9464c44e76cc706f120468bc9892fe1a3ca0435
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-11-06 16:35:55 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,20 @@
+function solution(s){
+    // 대소문자 구분하지 않음
+    // p의 개수 카운트, y의 개수 카운트
+    // 개수가 같고 모두 하나도 없으면 true
+    // 다르면 false
+    const pORy = {
+        p : 0,
+        y : 0,
+    }
+    
+    for(value of s.toUpperCase()){
+        if(value === 'P') ++pORy.p;
+        else if(value === "Y") ++pORy.y;
+    }
+    
+    if(pORy.p === pORy.y){
+        return true;
+    } 
+    return false;
+}
