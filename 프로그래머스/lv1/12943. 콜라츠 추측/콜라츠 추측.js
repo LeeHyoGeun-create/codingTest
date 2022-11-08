eb8192704a672b7635f51ef9c36ce5a3b8f0a6ac
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-11-08 21:34:04 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,17 @@
+function solution(num) {
+    // while문으로 반복하며 해당 작업이 한번 끝나면 count를 더한다
+    // count가 500번을 초과한다면 -1을 반환한다.
+    // num이 1이되면 count를 반환한다.
+    let count = 0;
+    const MAX_NUMBER = 500;
+    while(true){
+        if(count > MAX_NUMBER){
+            return -1;
+        }
+        if(num === 1){
+            return count;
+        }
+        num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+        count++;
+    }
+}
