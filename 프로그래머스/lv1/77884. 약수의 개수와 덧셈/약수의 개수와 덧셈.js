e817dcbc6d562b1858a2583a434aa36bb97d061a
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-11-10 21:59:00 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,10 +1,10 @@
-const cache = {};
+const cache = {'1' : 1, '2': 2, '3': 3, '4': 3, '5' : 2, '6' : 4, '7' : 2, '8' : 4, '9' : 2, '10' : 4};
 
 function solution(MIN_NUMBER_IN, MAX_NUMBER_IN) {
     // 각 수의 사이를 for문으로 돌며 값을 누적
     var answer = 0;
     for(let i = MIN_NUMBER_IN ; i <= MAX_NUMBER_IN ; i++){
-        console.log(cache, countDivisor(i));
+        // console.log(cache, countDivisor(i));
         if(isOdd(countDivisor(i))){
            answer -= i;
            continue;
@@ -24,6 +24,10 @@ function isOdd(num){
     return true;
 }
 
+// Number.isInteger(Math.sqrt(i)) 제곱근이 정수면 약수는 홀수
+// 16 1 2 4 8 16 약수는 대칭인데 
+// 제곱근이 정수면 4처럼 사이에 하나 들어감
+
 // 제곱근으로 범위를 줄임
 // 나눠지면 약수
     // cache에 값이 저장되어 있으면 count에 더하고 탐색 종료
@@ -31,18 +35,33 @@ function isOdd(num){
 // cache에 해당 약수가 없으면 값 등록
 function countDivisor(num){
     let count = 0;
-    for(let i = Math.floor(Math.sqrt(num)) ; i > 0  ; i--){  
+    for(let i = Math.floor(Math.sqrt(num)) ; i > 0  ; i--){        
         if(num % i === 0){
-            count++;
+            count++;   
             if(num / i !== i) count++;
-        }
-        // if(cache[i]){
-        //     count += cache[i];
-        //     break;
-        // }
+        }  
     }
-    // cache.num = cache.num ? cahe.num : count;
-    // https://www.youtube.com/watch?v=VhNVd1YEUd4
-    // cache[num] ??= count;
     return count;
 }
+
+
+
+// function countDivisor(num){
+//     let count = 0;
+//     for(let i = Math.floor(Math.sqrt(num)) ; i > 0  ; i--){  
+//     if(cache[i]){
+//             count += cache[i];
+//             break;
+//          }
+        
+//         if(num % i === 0){
+//             count++;   
+//             if(num / i !== i) count++;
+//         }
+        
+//     }
+//     // cache.num = cache.num ? cahe.num : count;
+//     // https://www.youtube.com/watch?v=VhNVd1YEUd4
+//     cache[num] ??= count;
+//     return count;
+// }
