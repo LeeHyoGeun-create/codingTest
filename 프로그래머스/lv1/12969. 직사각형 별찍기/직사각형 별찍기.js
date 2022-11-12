ffc2fdd0e83960e1cd0e58d75f93747e161cb497
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-11-12 12:53:36 +0900

    [level 1] Title: 직사각형 별찍기, Time: 472.12 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -11,3 +11,13 @@ process.stdin.on('data', data => {
     }
     console.log(string);
 });
+
+
+/*
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    console.log((('*').repeat(a)+`\n`).repeat(b))
+});
+*/
