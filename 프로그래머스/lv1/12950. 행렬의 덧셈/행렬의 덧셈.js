a047e0a8b8388c19b375b0f79a6ab12e95587d4f
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-11-12 13:46:26 +0900

    [level 1] Title: 행렬의 덧셈, Time: 40.75 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,16 @@
+function solution(arr1, arr2) {
+    // 배열 전부를 순회하며
+    // 값에 닿으면 해당 인덱스를 받아
+    // answer에 넣음.
+    var answer = [];
+    arr1.forEach((innerArr, outIndex)=>{
+        const newArr = [];
+        innerArr.forEach((value, innerIndex)=>{
+            newArr.push(arr1[outIndex][innerIndex] + arr2[outIndex][innerIndex]);
+        })
+        answer.push(newArr);
+    })
+    
+    
+    return answer;
+}
