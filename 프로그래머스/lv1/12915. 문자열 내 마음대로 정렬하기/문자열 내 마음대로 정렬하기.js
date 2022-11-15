19315a52219a06d9baa33ea1a21ecf0aebec27b5
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-11-16 01:14:27 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -9,15 +9,42 @@ function solution(a,b){
         return a
     }
 
+/*
+    
+function solution(strings, n) {
+    var answer = [];
+    strings.sort((a,b)=>{
+        if(a[n]>b[n]){
+            return 1
+        } else if(b[n]>a[n]){
+            return -1
+        } else if(a[n]==b[n]){
+            if(a>b){
+                return 1
+            } else if(a<b){
+                return -1
+            }   else{
+                return 0
+            }
+        }
+    })
+    return strings
+}
+*/
+
 
 /*
 function solution(strings, n) {
     // 최대한 sort로 해보기
     // 같은 문자가 나왔을 경우 인덱스를 앞에서부터 비교하며
     // 다른 값이 나온 인덱스로 비교
+    
     let answer = [];
    
-  
+    // 두 인덱스가 같은 경우
+    // 다시 밖으로 나와서 비교를 했어야 했는데
+    // 나는 내부로 중첩하며 들어가서
+    // 값이 정상적으로 나오지 않았다.
     answer = strings.sort((a,b)=> {
     if (a[n] < b[n]) return -1;
     else if (b[n] < a[n]) return 1;
