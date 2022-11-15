b2202bd41776e8b7ae1482f708fa13cdb1cd325f
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-11-16 00:33:21 +0900

    [level 1] Title: 최소직사각형, Time: 3.28 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,6 +1,6 @@
 function solution(sizes) {
-    // maxMax에는 배열의 큰 값 중에서 최대값
-    // minmax에는 배열의 작은 값 중에서 최대값
+    // firstMax에는 배열의 큰 값 중에서 최대값
+    // secondMax에는 배열의 작은 값 중에서 최대값
     // 각 위치의 최고 사이즈를 골라 곱하면 됨.
 
     let firstMax = 0;
