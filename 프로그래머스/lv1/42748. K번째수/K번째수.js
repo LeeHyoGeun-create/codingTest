5fd9e576affa03742024c27347cb86d2cd260a6a
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-11-17 22:24:22 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -3,10 +3,24 @@ function solution(array, commands) {
     // 정렬
     // k 번쨰에 있는 수 가져옴    
     const answer = [];
-    for(value of commands){
+    for(command of commands){
         // 번째를 조심해야한다...
-        const sortedArr = array.slice(value[0]-1,value[1]).sort((a,b)=>a-b);
-        answer.push(sortedArr[value[2]-1]);
+        const [sPosition, ePosition, position] = command
+        const sortedArr = array.slice(sPosition - 1, ePosition).sort((a,b)=>a-b);
+        answer.push(sortedArr[position - 1]);
     }
     return answer;
 }
+
+/*
+function solution(array, commands) {
+    return commands.map(command => {
+        const [sPosition, ePosition, position] = command
+        const newArray = array
+            .filter((value, fIndex) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1)
+            .sort((a,b) => a - b)    
+
+        return newArray[position - 1]
+    })
+}
+*/
