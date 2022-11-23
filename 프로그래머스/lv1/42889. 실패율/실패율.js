c496014d3730c6d2900a19d2df029db61b2b42c2
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-11-23 22:00:45 +0900

    [level 1] Title: 실패율, Time: 3.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -60,4 +60,18 @@ function solution(N, stages) {
 }
 
 
+/* 내장고차함수.... 
+function solution(N, stages) {
+    let result = [];
+    for(let i=1; i<=N; i++){
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        // 이런식으로 배열에 담아도 문제가 없구나.
+        result.push([i, curr/reach]);
+    }
+    result.sort((a,b) => b[1] - a[1]);
+    return result.map((x) => x[0]);
+}
 
+
+*/
