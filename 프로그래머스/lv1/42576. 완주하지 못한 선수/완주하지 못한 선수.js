a8a1e25064bc2fe0cebafc3bb10147739ef11d61
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-11-29 22:48:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 96.08 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,5 +1,40 @@
+/*
 function solution(participant, completion) {
-    // participant에서 completion을 지워나감
+    const map = new Map();
+
+    for(let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i];
+
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
+    }
+
+    for(let [k, v] of map) {
+        if(v > 0) return k;
+    }
+}
+*/
+
+// 배열을 객체처럼 사용
+// function solution(participant, completion){
+//     // const newObj = getObj(completion);
+//     // console.log(newObj)
+//     return participant.find((value)=> !completion[value]--, getObj(completion));
+// }
+
+// function getObj(arr){
+//         // 비트 or 연산자
+//         // (undefined | 0) + 1          1
+//         // ('eden' | 0) + 1             1
+//         // (1 | 1) + 1                  2
+//         // https://stackoverflow.com/questions/6194950/what-does-the-single-pipe-do-in-javascript
+//     arr.map((name)=> arr[name] = (arr[name] | 0) + 1);
+//     console.log(arr);
+// }
+
+
+function solution(participant, completion) {
     const sortedParticipant = participant.sort();
     const sortedCompletion  = completion.sort();
     for(let i = 0 ; i < sortedParticipant.length ; i++ ){
