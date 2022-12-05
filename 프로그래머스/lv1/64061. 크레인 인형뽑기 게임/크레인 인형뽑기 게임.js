f2fc14ad639c818c82b77a07e39a1f0015abab96
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-12-05 14:58:26 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.61 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,3 +1,55 @@
+/*
+function minSubArrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+ 
+  while (start < nums.length) {
+    // if current window doesn't add up to the given sum then 
+		// move the window to right
+    if(total < sum && end < nums.length){
+      total += nums[end];
+			end++;
+    }
+    // if current window adds up to at least the sum given then
+		// we can shrink the window 
+    else if(total >= sum){
+      minLen = Math.min(minLen, end-start);
+			total -= nums[start];
+			start++;
+    } 
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+*/
+
+// const transpose = matrix =>
+//     matrix.reduce(
+//         (result, row) => row.map((_, i) => [...(result[i] || []), row[i]]),
+//         []
+//     );
+
+// const solution = (board, moves) => {
+//     const stacks = transpose(board).map(row =>
+//         row.reverse().filter(el => el !== 0)
+//     );
+//     const basket = [];
+//     let result = 0;
+
+//     for (const move of moves) {
+//         const pop = stacks[move - 1].pop();
+//         if (!pop) continue;
+//         if (pop === basket[basket.length - 1]) {
+//             basket.pop();
+//             result += 2;
+//             continue;
+//         }
+//         basket.push(pop);
+//     }
+
+//     return result;
+// };
+
+
 function solution(board, moves) {
     // 같은 모양 2개면 사라짐
     // 인덱스와 같은 key 값을 갖는 객체를 만든 후
@@ -38,3 +90,6 @@ function solution(board, moves) {
     
     return answer;
 }
+
+
+
