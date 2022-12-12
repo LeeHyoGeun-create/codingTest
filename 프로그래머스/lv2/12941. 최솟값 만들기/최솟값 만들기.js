c53d4b733cc9a51413034a2bb5bf81ab6ee477eb
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-12-13 00:04:37 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.80 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -3,8 +3,8 @@ function solution(A,B){
     // A를 오름차순으로 정렬하고 B를 내림차순으로 정렬한 후
     // for문을 돌며 같은 인덱스에 있는 값의 곱들을 누적시켜감
     let answer = 0;
-    A = A.sort((a,b)=>a-b);
-    B = B.sort((a,b)=>b-a);
+    A.sort((a,b)=>a-b);
+    B.sort((a,b)=>b-a);
     for(let i = 0 ; i < A.length ; i++){
         answer += A[i] * B[i];
     }
