f66cc6a31509957d1e978bd5e4c565b9b72fd367
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2022-12-19 18:44:28 +0900

    [level 2] Title: 올바른 괄호, Time: 8.67 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,9 +1,5 @@
 function solution(s){
-   // 스텍을 만들고
-   // 스택의 끝 부분을 조사해서
-   // "()" 쌍이 맞춰질 경우 poppop;
-   // 스택이 비어있으면 true
-   // 아니면 false
+  
     const stack = [];
     for(const value of s){  
         stack.push(value);
@@ -11,8 +7,7 @@ function solution(s){
             stack.pop();
             stack.pop();
             continue;
-        }  
-        
+        }     
     }
     return stack.length === 0 ? true : false;
 }
