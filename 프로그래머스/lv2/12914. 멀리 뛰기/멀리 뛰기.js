71c7c93ae16801dd484776c9588202c5d384cb2a
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2023-01-01 11:08:19 +0900

    [level 2] Title: 멀리 뛰기, Time: 704.87 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,34 @@
+const factorial = (n) => {
+    if(n <= 0) return BigInt(1);
+    let num = BigInt(1);
+    let a = BigInt(n);
+    while(a != BigInt(1)){
+        num *= a ;
+        a = a -  1n;
+    }
+    return num ;
+}
+
+function solution(n) {
+    // n개를 나열하는 방법 n!/ 같은거 3개를 나열하는 법 3!, 같은거 2개를 나열하는 법 2!
+    // 2 2, 1, 1, 1 
+    // 5!/(3! * 2!) = 10
+    // 묶음을 구하는 공식?
+    // 두개씩 뽑는 경우 n / 2 
+    // n이 4일 경우 2, 1, 0 묶음 수가 다음과 같이 선택됨.
+    // 2, 2 2
+    // 1, 1 1 2, 1 2 1, 2 1 1
+    // 0, 1 1 1 1
+    // n 3
+    // 1, 2 1, 2 1
+    // 0  1 1 1
+    let answer = BigInt(0);
+    const maxTwoNum = Math.floor(n / 2);
+    for(let twoNum = 0 ; twoNum <= maxTwoNum ; twoNum++){
+        const oneNum = n - (twoNum * 2)
+        const length = twoNum + oneNum;
+        const value = factorial(length) / (factorial(oneNum) * factorial(twoNum));
+        answer += value;
+    }
+    return Number(answer % BigInt(1234567));
+}
