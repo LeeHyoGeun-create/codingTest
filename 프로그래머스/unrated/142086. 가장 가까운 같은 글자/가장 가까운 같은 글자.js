098a23d562541316f7fa2d4bb7471724f2c1022f
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2023-01-14 19:11:31 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 121.57 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,28 @@
+const plusObjectValues = (obj) => {
+    for(const [key, value] of Object.entries(obj)){
+        obj[key] = value + 1;
+    } 
+    return;
+}
+
+function solution(s) {
+    // 처음 만나는 문자 (객체의 키값으로 여부 확인) 0으로 초기화하고
+    // answer 배열에 -1 push
+    // 한칸씩 움직일 때 마다 그 객체의 모든 요소에 +1
+    // 해당하는 값을 만났을 때 answer 배열에 들어있는 값을 넣고
+    // 0으로 초기화
+    const answer = [];
+    const obj = {};
+    for(const char of s){
+        if(char in obj){
+            answer.push(obj[char]);
+            obj[char] = 0;
+        } else{
+            answer.push(-1);
+            obj[char] = 0;
+        }
+        
+        plusObjectValues(obj);
+    }
+    return answer;
+}
