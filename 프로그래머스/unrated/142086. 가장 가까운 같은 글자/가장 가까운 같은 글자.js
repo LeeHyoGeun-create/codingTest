d3234c556312b4a43645af09647fda4a1a85c20b
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2023-01-14 19:14:18 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 87.58 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -26,3 +26,15 @@ function solution(s) {
     }
     return answer;
 }
+
+/*
+function solution(s) {
+    const hash={};
+
+    return [...s].map((v,i)=>{
+        let result = hash[v] !== undefined ? i - hash[v] : -1;
+        hash[v] = i;
+        return result;
+    });
+}
+*/
