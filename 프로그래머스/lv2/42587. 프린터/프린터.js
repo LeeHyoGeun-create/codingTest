2adfcba0bb9abe8c25b75e55b02da3f752b357bf
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2023-01-16 18:41:57 +0900

    [level 2] Title: 프린터, Time: 4.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.js
+++ 프로그래머스/lv2/42587. 프린터/프린터.js
@@ -1,41 +1,46 @@
-function solution(priorities, location) {
-    var list = priorities.map((t,i)=>({
-        my : i === location,
-        val : t
-    }));
-    var count = 0;        
-    while(true){
-        var cur = list.splice(0,1)[0];        
-        if(list.some(t=> t.val > cur.val )){
-            list.push(cur);                        
-        }
-        else{            
-            count++;
-            if(cur.my) return count;
-        }
-    }
-}
-
-// function solution(priorities, location) { 
-//     let answer = 0;
-//     const sortedPriorities = [...priorities].sort((a,b)=> a-b);
-//     priorities = priorities.map(value=>[value]);
-//     priorities[location].push(1);
-//     let prioritiesLength = priorities.length;
-//     let index = 0;
-//     let end = undefined; 
-//     while(end === undefined){      
-//         if(priorities[index][0] >= sortedPriorities.at(-1)){
-//             answer++;
-//             sortedPriorities.pop();
-//             end = priorities.splice(index,1)[0][1];     
-//             prioritiesLength = priorities.length;
-//             index--;
+// function solution(priorities, location) {
+//     var list = priorities.map((t,i)=>({
+//         my : i === location,
+//         val : t
+//     }));
+//     var count = 0;        
+//     while(true){
+//         var cur = list.splice(0,1)[0];        
+//         if(list.some(t=> t.val > cur.val )){
+//             list.push(cur);                        
 //         }
-//         index = ++index % prioritiesLength;
+//         else{            
+//             count++;
+//             if(cur.my) return count;
+//         }
 //     }
-//     return answer;
 // }
+
+function solution(priorities, location) { 
+    // shift, push를 하지 않기위해 index로 순회
+    // 최대값을 추적하기 위해 높은 순으로 정렬된 배열을 만들음
+    // 끝 값과 중요도를 비교하고 출력을 하게될 경우 하나씩 pop
+    // 원하는 값을 출력헀다는 표시를 하기 위해 배열로 바꾸고 새로운 값을 push
+    let answer = 0;
+    const sortedPriorities = [...priorities].sort((a,b)=> a-b);
+    priorities = priorities.map(value=>[value]);
+    priorities[location].push(1);
+    console.log(priorities);
+    let prioritiesLength = priorities.length;
+    let index = 0;
+    let end = undefined; 
+    while(end === undefined){      
+        if(priorities[index][0] >= sortedPriorities.at(-1)){
+            answer++;
+            sortedPriorities.pop();
+            end = priorities.splice(index,1)[0][1];     
+            prioritiesLength = priorities.length;
+            index--;
+        }
+        index = ++index % prioritiesLength;
+    }
+    return answer;
+}
 
 // function solution(priorities, location) {
 //     // 높은 가중치가 먼저 인쇄됨
