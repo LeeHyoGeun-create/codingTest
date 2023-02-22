38039b391fa23fb0c6a96232a7c5b160c89f56ec
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2023-02-22 20:04:38 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 89.58 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,21 +1,46 @@
-const getSum = (number , arr) => {
-    const sumArr = [];
-    for(let i = 0; i < arr.length; i++){
-        let sum = 0;
-        for(let j = 0; j < number; j++){
-            sum += arr[(i + j) % arr.length];
-        }
-        sumArr.push(sum);
-    }
-    return sumArr;
-}
+// const getSum = (number , arr) => {
+//     const sumArr = [];
+//     for(let i = 0; i < arr.length; i++){
+//         let sum = 0;
+//         for(let j = 0; j < number; j++){
+//             sum += arr[(i + j) % arr.length];
+//         }
+//         sumArr.push(sum);
+//     }
+//     return sumArr;
+// }
 
+// function solution(elements) {
+//     let mySet = new Set();
+    
+//     for(let i = 0; i < elements.length; i++){
+//         getSum(i + 1,elements).map(item => mySet.add(item));
+//     }
+    
+//     return mySet.size;
+// }
+
+
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
 function solution(elements) {
-    let mySet = new Set();
-    
-    for(let i = 0; i < elements.length; i++){
-        getSum(i + 1,elements).map(item => mySet.add(item));
+    const circular = elements.concat(elements);
+    const set = new Set();
+    for (let i = 0; i < elements.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < elements.length; j++) {
+            sum += circular[i + j];
+            set.add(sum);
+        }
     }
-    
-    return mySet.size;
+    return set.size;
 }
