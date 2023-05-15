bf35010a0ec6fd1a473c72fb37bd80e2a981e6b1
LeeHyoGeun-create <74353528+LeeHyoGeun-create@users.noreply.github.com>
2023-05-15 19:18:59 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 16.59 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,15 @@
+function solution(arr, queries) {
+    const answer = [];
+    for(const [s,e,k] of queries){
+        const temp = [];
+        for(let i = s; i <= e ; i++){
+            if(arr[i] > k){
+                temp.push(arr[i]);
+            }
+        }
+        const min = Math.min(...temp);
+        console.log(min);
+        answer.push(isFinite(min) ? min : -1);
+    }
+    return answer;
+}
